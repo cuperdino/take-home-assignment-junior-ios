631d81496fee3e2953ea1266776e5f201fa3b47b
Sabahudin Kodro <sabahudin.kodro@outlook.com>
2020-06-27 19:30:44 +0200

    Create NetworkService file

---
+++ EasyShopper/EasyShopper/NetworkService.swift
@@ -0,0 +1,19 @@
+//
+//  NetworkService.swift
+//  EasyShopper
+//
+//  Created by Sabahudin Kodro on 27/06/2020.
+//  Copyright © 2020 Ka-ching. All rights reserved.
+//
+
+import Foundation
+
+protocol NetworkServiceProtocol {
+    func getProducts()
+}
+
+class NetworkService: NetworkServiceProtocol {
+    func getProducts() {
+        
+    }
+}
