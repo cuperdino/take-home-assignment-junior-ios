1eac1ad62f9c8475fe6e2166a5dfe9bf52ea8644
Sabahudin Kodro <sabahudin.kodro@outlook.com>
2020-06-27 21:11:37 +0200

    Add ShoppingItemTableViewCell

---
+++ EasyShopper/EasyShopper/Views/TableViewCells/ShoppingItemTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ShoppingItemTableViewCell.swift
+//  EasyShopper
+//
+//  Created by Sabahudin Kodro on 27/06/2020.
+//  Copyright © 2020 Ka-ching. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingItemTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var shoppingItemImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var quantityLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
